536fb2fe928aff0200ec18562a2daee7ed0cafca
chingudl7 <chingudl7@naver.com>
2026-03-03 20:58:54 +0900

    docs(swagger): CursorPageRequest @Schema 추가 및 보강 작업계획 확정

--- src/main/java/com/link/vibe/global/common/CursorPageRequest.java
+++ src/main/java/com/link/vibe/global/common/CursorPageRequest.java
@@ -1,5 +1,6 @@
 package com.link.vibe.global.common;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +8,10 @@ import lombok.Setter;
 @Setter
 public class CursorPageRequest {
 
+    @Schema(description = "다음 페이지 커서 (첫 페이지는 미전달)", example = "42")
     private String cursor;
+
+    @Schema(description = "페이지 크기 (기본값 20, 최대 100)", example = "20")
     private int size = 20;
 
     public static final int MAX_SIZE = 100;
